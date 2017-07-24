9345faf7d33fd3bbb1f3e74d33ae44d70020831b
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-24 11:13:18 -0700

    Add a naive number implementation that uses doubles.

---
+++ src/org/nwapw/abacus/number/NaiveNumber.java
@@ -0,0 +1,39 @@
+package org.nwapw.abacus.number;
+
+public class NaiveNumber implements Number {
+
+    private double value;
+
+    public NaiveNumber(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public Number multiply(Number multiplier) {
+        if(!(multiplier instanceof NaiveNumber)) throw new IllegalArgumentException();
+        return new NaiveNumber(value * ((NaiveNumber)multiplier).value);
+    }
+
+    @Override
+    public Number divide(Number divisor) {
+        if(!(divisor instanceof NaiveNumber)) throw new IllegalArgumentException();
+        return new NaiveNumber(value * ((NaiveNumber)divisor).value);
+    }
+
+    @Override
+    public Number add(Number summand) {
+        if(!(summand instanceof NaiveNumber)) throw new IllegalArgumentException();
+        return new NaiveNumber(value * ((NaiveNumber)summand).value);
+    }
+
+    @Override
+    public Number subtract(Number subtrahend) {
+        if(!(subtrahend instanceof NaiveNumber)) throw new IllegalArgumentException();
+        return new NaiveNumber(value * ((NaiveNumber)subtrahend).value);
+    }
+
+    @Override
+    public Number negate() {
+        return new NaiveNumber(-value);
+    }
+}
