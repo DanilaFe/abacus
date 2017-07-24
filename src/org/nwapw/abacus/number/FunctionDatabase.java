3b913041751bcec62e255e2312ca0832e0ae994d
Arthur Drobot <drobotarthur@gmail.com>
2017-07-24 14:48:16 -0700

    Add the four external essential functions.

--- src/org/nwapw/abacus/number/FunctionDatabase.java
+++ src/org/nwapw/abacus/number/FunctionDatabase.java
@@ -8,10 +8,66 @@ public class FunctionDatabase {
 
     private void registerDefault(){
 
+        functions.put("+", new Function() {
+            @Override
+            protected boolean matchesParams(Number[] params) {
+                return params.length >= 1;
+            }
+
+            @Override
+            protected Number applyInternal(Number[] params) {
+                Number sum = params[0];
+                for(int i = 1; i < params.length; i++){
+                    sum = sum.add(params[i]);
+                }
+                return sum;
+            }
+        });
+
+        functions.put("-", new Function() {
+            @Override
+            protected boolean matchesParams(Number[] params) {
+                return params.length == 2;
+            }
+
+            @Override
+            protected Number applyInternal(Number[] params) {
+                return params[0].subtract(params[1]);
+            }
+        });
+
+        functions.put("*", new Function() {
+            @Override
+            protected boolean matchesParams(Number[] params) {
+                return params.length >= 1;
+            }
+
+            @Override
+            protected Number applyInternal(Number[] params) {
+                Number product = params[1];
+                for(int i = 1; i < params.length; i++){
+                    product = product.multiply(params[i]);
+                }
+                return product;
+            }
+        });
+
+        functions.put("/", new Function() {
+            @Override
+            protected boolean matchesParams(Number[] params) {
+                return params.length == 2;
+            }
+
+            @Override
+            protected Number applyInternal(Number[] params) {
+                return params[0].divide(params[1]);
+            }
+        });
     }
 
     public FunctionDatabase(){
         functions = new HashMap<>();
+        registerDefault();
     }
 
     public Function getFunction(String name){
