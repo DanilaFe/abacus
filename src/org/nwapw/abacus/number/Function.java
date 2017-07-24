7bab3773dc37c430d8e4ce833c29dc7799c15005
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-24 14:26:09 -0700

    Add a map to promoting priorities.

--- src/org/nwapw/abacus/number/Function.java
+++ src/org/nwapw/abacus/number/Function.java
@@ -1,6 +1,13 @@
 package org.nwapw.abacus.number;
 
+import java.util.HashMap;
+
 public abstract class Function {
+
+    private static final HashMap<Class<? extends Number>, Integer> priorityMap =
+            new HashMap<Class<? extends Number>, Integer>() {{
+                put(NaiveNumber.class, 0);
+            }};
 
     protected abstract boolean matchesParams(Number[] params);
     protected abstract Number applyInternal(Number[] params);
