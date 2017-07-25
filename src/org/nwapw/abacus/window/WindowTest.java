27cf6ce64b9c6c5f55337869252909e181e0dd90
rileyJones <rileyjones109@gmail.com>
2017-07-25 14:52:57 -0700

    Add GUI

---
+++ src/org/nwapw/abacus/window/WindowTest.java
@@ -0,0 +1,24 @@
+package org.nwapw.abacus.window;
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.util.Arrays;
+public class WindowTest {
+    public static void main(String[] args){
+
+
+
+
+
+
+
+
+
+        new Window().setVisible(true);
+
+    }
+}
