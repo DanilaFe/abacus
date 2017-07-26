c14a4ceb9329564f247034684fd7cf70cc2f86e2
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-25 21:52:23 -0700

    Add the plugin manager to the main. Abacus class.

--- src/org/nwapw/abacus/Abacus.java
+++ src/org/nwapw/abacus/Abacus.java
@@ -1,5 +1,7 @@
 package org.nwapw.abacus;
 
+import org.nwapw.abacus.plugin.PluginManager;
+import org.nwapw.abacus.plugin.StandardPlugin;
 import org.nwapw.abacus.window.Window;
 
 import javax.swing.*;
@@ -7,6 +9,7 @@ import javax.swing.*;
 public class Abacus {
 
     private Window mainUi;
+    private PluginManager manager;
 
     public Abacus(){
         init();
@@ -18,6 +21,8 @@ public class Abacus {
         } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException e) {
             e.printStackTrace();
         }
+        manager = new PluginManager();
+        manager.addInstantiated(new StandardPlugin(manager));
         mainUi = new Window();
         mainUi.setVisible(true);
     }
