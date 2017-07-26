734256fac1ecbb0c99100f5cab5b4c63dfab4637
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-25 21:11:36 -0700

    Make the program actually create the UI.

--- src/org/nwapw/abacus/Abacus.java
+++ src/org/nwapw/abacus/Abacus.java
@@ -1,9 +1,29 @@
 package org.nwapw.abacus;
 
+import org.nwapw.abacus.window.Window;
+
+import javax.swing.*;
+
 public class Abacus {
 
+    private Window mainUi;
+
+    public Abacus(){
+        init();
+    }
+
+    private void init() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        mainUi = new Window();
+        mainUi.setVisible(true);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        new Abacus();
     }
 
 }
