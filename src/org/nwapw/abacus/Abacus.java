408c4f0379696a29e3e7265ae32270776ab0d80d
rileyJones <galbraithja@beaverton.k12.or.us>
2017-07-28 11:17:54 -0700

    Add external plugin support

--- src/org/nwapw/abacus/Abacus.java
+++ src/org/nwapw/abacus/Abacus.java
@@ -1,10 +1,14 @@
 package org.nwapw.abacus;
 
 import org.nwapw.abacus.plugin.PluginManager;
-import org.nwapw.abacus.plugin.StandardPlugin;
+//import org.nwapw.abacus.plugin.StandardPlugin;
 import org.nwapw.abacus.window.Window;
+import org.nwapw.abacus.plugin.ClassFinderV2;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Abacus {
 
@@ -22,10 +26,33 @@ public class Abacus {
             e.printStackTrace();
         }
         manager = new PluginManager();
-        manager.addInstantiated(new StandardPlugin(manager));
+        ArrayList<String> names = new ArrayList();
+        try {
+
+            ClassFinderV2 classFinder = new ClassFinderV2();
+            File pluginFile = new File("C:\\Users\\galbraithja\\Desktop\\.git\\abacus\\src\\org\\nwapw\\abacus\\plugin");
+            for(File classes:pluginFile.listFiles()){
+                if(classes.getName().endsWith(".jar")){
+                    names.addAll(classFinder.addJar("C:\\Users\\galbraithja\\Desktop\\.git\\abacus\\src\\org\\nwapw\\abacus\\plugin\\Standard.jar"));
+                }
+            }
+            for(String name:names){
+                System.out.println(name);
+            }
+            ArrayList<Class> classes = classFinder.getClasses();
+            for(Class classGet:classes){
+                manager.addClass(classGet);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         mainUi = new Window(manager);
         mainUi.setVisible(true);
         manager.load();
+
     }
 
     public static void main(String[] args){
