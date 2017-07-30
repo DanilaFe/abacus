7b2ee1c87a4ac843f832d2a5411c9af74219538d
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-29 21:49:36 -0700

    Create empty class, getting ready to write tests.

---
+++ src/test/java/org/nwapw/abacus/tests/LexerTests.java
@@ -0,0 +1,12 @@
+package org.nwapw.abacus.tests;
+
+import org.nwapw.abacus.lexing.Lexer;
+import org.nwapw.abacus.tree.TokenType;
+
+public class LexerTests {
+
+    Lexer<TokenType> lexer = new Lexer<TokenType>(){{
+
+    }};
+    
+}
