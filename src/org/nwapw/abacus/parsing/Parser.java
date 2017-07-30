dcbda5b255289a43d485595ece765431a2c3cdfa
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-29 21:36:39 -0700

    Add comments to the two parsing interfaces.

--- src/org/nwapw/abacus/parsing/Parser.java
+++ src/org/nwapw/abacus/parsing/Parser.java
@@ -4,7 +4,17 @@ import org.nwapw.abacus.tree.TreeNode;
 
 import java.util.List;
 
+/**
+ * An itnerface that provides the ability to convert a list of tokens
+ * into a parse tree.
+ * @param <T> the type of tokens accepted by this parser.
+ */
 public interface Parser<T> {
 
+    /**
+     * Constructs a tree out of the given tokens.
+     * @param tokens the tokens to construct a tree from.
+     * @return the constructed tree, or null on error.
+     */
     public TreeNode constructTree(List<T> tokens);
 }
--- src/org/nwapw/abacus/parsing/Tokenizer.java
+++ src/org/nwapw/abacus/parsing/Tokenizer.java
@@ -2,8 +2,17 @@ package org.nwapw.abacus.parsing;
 
 import java.util.List;
 
+/**
+ * Interface that provides the ability to convert a string into a list of tokens.
+ * @param <T> the type of the tokens produced.
+ */
 public interface Tokenizer<T> {
 
+    /**
+     * Converts a string into tokens.
+     * @param string the string to convert.
+     * @return the list of tokens, or null on error.
+     */
     public List<T> tokenizeString(String string);
 
 }
