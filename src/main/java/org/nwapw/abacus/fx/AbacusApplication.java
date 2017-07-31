0b173a68dd961d691422ad481a1e1b1631b80100
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-31 16:50:39 -0700

    Add FXML loading application.

---
+++ src/main/java/org/nwapw/abacus/fx/AbacusApplication.java
@@ -0,0 +1,20 @@
+package org.nwapw.abacus.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AbacusApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("/abacus.fxml"));
+        Scene mainScene = new Scene(parent, 320, 480);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Abacus");
+        primaryStage.show();
+    }
+
+}
