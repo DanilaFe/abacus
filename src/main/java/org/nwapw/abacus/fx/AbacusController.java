44ed0199d4d3ddec54bf5c8db268b873f6a713c3
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-31 16:52:34 -0700

    Add initialization code to AbacusController

--- src/main/java/org/nwapw/abacus/fx/AbacusController.java
+++ src/main/java/org/nwapw/abacus/fx/AbacusController.java
@@ -1,5 +1,15 @@
 package org.nwapw.abacus.fx;
 
+import javafx.fxml.FXML;
+import org.nwapw.abacus.Abacus;
+
 public class AbacusController {
+
+    private Abacus abacus;
+
+    @FXML
+    public void initialize(){
+        abacus = new Abacus();
+    }
 
 }
