4be4ffd57a80808a331796a4a05540adad1668ff
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-01 10:47:31 -0700

    Add comments to the JavaFX codebase

--- src/main/java/org/nwapw/abacus/fx/HistoryModel.java
+++ src/main/java/org/nwapw/abacus/fx/HistoryModel.java
@@ -3,12 +3,33 @@ package org.nwapw.abacus.fx;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * The data model used for storing history entries.
+ */
 public class HistoryModel {
 
+    /**
+     * The property used for displaying the column
+     * for the user input.
+     */
     private final StringProperty input;
+    /**
+     * The property used for displaying the column
+     * that contains the parsed input.
+     */
     private final StringProperty parsed;
+    /**
+     * The property used for displaying the column
+     * that contains the program output.
+     */
     private final StringProperty output;
 
+    /**
+     * Creates a new history model with the given variables.
+     * @param input the user input
+     * @param parsed the parsed input
+     * @param output the program output.
+     */
     public HistoryModel(String input, String parsed, String output){
         this.input = new SimpleStringProperty();
         this.parsed = new SimpleStringProperty();
@@ -18,23 +39,47 @@ public class HistoryModel {
         this.output.setValue(output);
     }
 
+    /**
+     * Gets the input property.
+     * @return the input property.
+     */
     public StringProperty inputProperty() {
         return input;
     }
+    /**
+     * Gets the input.
+     * @return the input.
+     */
     public String getInput() {
         return input.get();
     }
 
+    /**
+     * Gets the parsed input property.
+     * @return the parsed input property.
+     */
     public StringProperty parsedProperty() {
         return parsed;
     }
+    /**
+     * Gets the parsed input.
+     * @return the parsed input.
+     */
     public String getParsed() {
         return parsed.get();
     }
 
+    /**
+     * Gets the output property.
+     * @return the output property.
+     */
     public StringProperty outputProperty() {
         return output;
     }
+    /**
+     * Gets the program output.
+     * @return the output.
+     */
     public String getOutput() {
         return output.get();
     }
