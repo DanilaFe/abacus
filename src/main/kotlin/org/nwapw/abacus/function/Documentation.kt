ea5ff08c09d9ebf20b5065c94907f1dbf7425c7c
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-08 10:54:36 -0700

    Add documentation type for a documentation entry.

--- src/main/kotlin/org/nwapw/abacus/function/Documentation.kt
+++ src/main/kotlin/org/nwapw/abacus/function/Documentation.kt
@@ -10,6 +10,8 @@ package org.nwapw.abacus.function
  * @param name the name of the function in English.
  * @param description the short description of this function.
  * @param longDescription the full description of this function.
+ * @param type the things this documentation maps to.
  */
 data class Documentation(val codeName: String, val name: String,
-                         val description: String, val longDescription: String)
+                         val description: String, val longDescription: String,
+                         val type: DocumentationType)
