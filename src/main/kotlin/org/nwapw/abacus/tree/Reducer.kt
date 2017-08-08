536cac7b23a9efe18ed4cd4d84f468d94484b2b3
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-08 10:11:30 -0700

    Convert Reducer interface to Kotlin.

---
+++ src/main/kotlin/org/nwapw/abacus/tree/Reducer.kt
@@ -0,0 +1,19 @@
+package org.nwapw.abacus.tree
+
+/**
+ * Reducer interface that takes a tree and returns a single value.
+ *
+ * The reducer walks the tree, visiting the children first, converting them into
+ * a value, and then attempts to reduce the parent. Eventually, the single final value is returned.
+ */
+interface Reducer <out T> {
+
+    /**
+     * Reduces the given tree node, given its already reduced children.
+     *
+     * @param treeNode the tree node to reduce.
+     * @param children the list of children, of type T.
+     */
+    fun reduceNode(treeNode: TreeNode, vararg children: Any) : T?
+
+}
