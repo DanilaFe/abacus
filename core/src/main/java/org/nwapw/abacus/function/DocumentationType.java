16faceb3ccd07c7bb7b23a564544bef72e0eb4bd
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-16 15:38:02 -0700

    Add comment to DocumentationType.

--- core/src/main/java/org/nwapw/abacus/function/DocumentationType.java
+++ core/src/main/java/org/nwapw/abacus/function/DocumentationType.java
@@ -1,5 +1,9 @@
 package org.nwapw.abacus.function;
 
+/**
+ * Enum that holds the type of documentation that has been
+ * registered with Abacus.
+ */
 public enum DocumentationType {
 
     FUNCTION
