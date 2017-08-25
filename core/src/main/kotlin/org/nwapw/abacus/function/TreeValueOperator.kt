efd283aaf1e54b836871cba5d692148d1d940509
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-25 15:49:35 -0700

    Add a TreeValueOperator.

---
+++ core/src/main/kotlin/org/nwapw/abacus/function/TreeValueOperator.kt
@@ -0,0 +1,15 @@
+package org.nwapw.abacus.function
+
+import org.nwapw.abacus.number.NumberInterface
+import javax.swing.tree.TreeNode
+
+/**
+ * An operator that operates on trees.
+ *
+ * This operator operates on parse trees, returning, however a number.
+ * @param associativity the associativity of the operator.
+ * @param type the type of the operator (infix, postfix, etc)
+ * @param precedence the precedence of the operator.
+ */
+abstract class TreeValueOperator(associativity: OperatorAssociativity, type: OperatorType,
+                                 precedence: Int) : ReducerApplicable<TreeNode, NumberInterface, NumberInterface>
