f96f809abf01bb62c40aff8213fc56d7d454cfd0
Danila Fedorin <danila.fedorin@gmail.com>
2017-08-25 00:43:36 -0700

    Abstract some of the Function functionality further.

---
+++ core/src/main/java/org/nwapw/abacus/function/Applicable.java
@@ -0,0 +1,38 @@
+package org.nwapw.abacus.function;
+
+/**
+ * A class that represents something that can be applied to one or more
+ * arguments of the same type, and returns a single value from that application.
+ * @param <T> the type of the parameters passed to this applicable.
+ * @param <O> the return type of the applicable.
+ */
+public abstract class Applicable<T, O> {
+
+    /**
+     * Checks if the given applicable can be used with the given parameters.
+     * @param params the parameter array to verify for compatibility.
+     * @return whether the array can be used with applyInternal.
+     */
+    protected abstract boolean matchesParams(T[] params);
+
+    /**
+     * Applies the applicable object to the given parameters,
+     * without checking for compatibility.
+     * @param params the parameters to apply to.
+     * @return the result of the application.
+     */
+    protected abstract O applyInternal(T[] params);
+
+    /**
+     * If the parameters can be used with this applicable, returns
+     * the result of the application of the applicable to the parameters.
+     * Otherwise, returns null.
+     * @param params the parameters to apply to.
+     * @return the result of the operation, or null if parameters do not match.
+     */
+    public O apply(T... params){
+        if(!matchesParams(params)) return null;
+        return applyInternal(params);
+    }
+
+}
