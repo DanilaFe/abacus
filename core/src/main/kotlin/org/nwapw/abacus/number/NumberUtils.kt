e62722ce2f1175a5d665bf1df7507509345b1d0e
Danila Fedorin <danila.fedorin@gmail.com>
2017-09-01 18:32:41 -0700

    Add comments.

--- core/src/main/kotlin/org/nwapw/abacus/number/NumberUtils.kt
+++ core/src/main/kotlin/org/nwapw/abacus/number/NumberUtils.kt
@@ -5,6 +5,12 @@ typealias PromotionFunction = java.util.function.Function<NumberInterface, Numbe
 typealias PromotionPath = List<PromotionFunction>
 typealias NumberClass = Class<NumberInterface>
 
+/**
+ * Promote a number through this path. The functions in this path
+ * are applied in order to the number, and the final result is returned.
+ *
+ * @param from the number to start from.
+ */
 fun PromotionPath.promote(from: NumberInterface): NumberInterface {
     return fold(from, { current, function -> function.apply(current) })
 }
--- core/src/main/kotlin/org/nwapw/abacus/number/PromotionResult.kt
+++ core/src/main/kotlin/org/nwapw/abacus/number/PromotionResult.kt
@@ -2,4 +2,10 @@ package org.nwapw.abacus.number
 
 import org.nwapw.abacus.plugin.NumberImplementation
 
+/**
+ * The result of promoting an array of NumberInterfaces.
+ *
+ * @param promotedTo the implementation to which the numbers were promoted.
+ * @param items the items the items resulting from the promotion.
+ */
 data class PromotionResult(val promotedTo: NumberImplementation, val items: Array<NumberInterface>)
