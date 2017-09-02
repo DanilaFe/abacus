28802cfed3621b3e6667d5c4fc159013d8e46531
Danila Fedorin <danila.fedorin@gmail.com>
2017-09-02 00:19:44 -0700

    Remove the additional methods from the VariableDatabase.

--- core/src/main/kotlin/org/nwapw/abacus/variables/VariableDatabase.kt
+++ core/src/main/kotlin/org/nwapw/abacus/variables/VariableDatabase.kt
@@ -4,7 +4,7 @@ import org.nwapw.abacus.Abacus
 import org.nwapw.abacus.number.NumberInterface
 import org.nwapw.abacus.plugin.PluginListener
 import org.nwapw.abacus.plugin.PluginManager
-import javax.swing.tree.TreeNode
+import org.nwapw.abacus.tree.TreeNode
 
 /**
  * A database for variables and definition.
@@ -19,53 +19,11 @@ class VariableDatabase(val abacus: Abacus): PluginListener {
     /**
      * The variables that are stored in the database.
      */
-    private val variables = mutableMapOf<String, NumberInterface>()
+    val variables = mutableMapOf<String, NumberInterface>()
     /**
      * The definitions that are stored in the database.
      */
-    private val definitions = mutableMapOf<String, TreeNode>()
-
-    /**
-     * Stores a variable in the database.
-     *
-     * @param name the name of the variable.
-     * @param value the new value of the variable.
-     */
-    fun storeVariable(name: String, value: NumberInterface) {
-        variables[name] = value
-    }
-
-    /**
-     * Stores a definition in the database
-     *
-     * @param name the name of the definition.
-     * @param value the new value of the definition.
-     */
-    fun storeDefinition(name: String, value: TreeNode) {
-        definitions[name] = value
-    }
-
-    /**
-     * Gets the value of the variable, or 0 if
-     * it is not defined.
-     *
-     * @param name the name of the variable.
-     * @return the value of the variable.
-     */
-    fun getVariableValue(name: String): NumberInterface {
-        return variables[name] ?:
-                abacus.numberImplementation.instanceForString("0")
-    }
-
-    /**
-     * Gets the definition.
-     *
-     * @param name the name of the definition.
-     * @return the value of the definition, or null if one doesn't exist.
-     */
-    fun getDefinition(name: String): TreeNode? {
-        return definitions[name]
-    }
+    val definitions = mutableMapOf<String, TreeNode>()
 
     override fun onLoad(manager: PluginManager?) {
 
