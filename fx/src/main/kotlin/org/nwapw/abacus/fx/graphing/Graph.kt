3bffa1c78f85f1995881c55ba2d29a9e88c6f485
Danila Fedorin <danila.fedorin@gmail.com>
2017-09-17 22:30:11 -0700

    Add a function to run inputs through an abacus expression.

--- fx/src/main/kotlin/org/nwapw/abacus/fx/graphing/Graph.kt
+++ fx/src/main/kotlin/org/nwapw/abacus/fx/graphing/Graph.kt
@@ -1,7 +1,12 @@
 package org.nwapw.abacus.fx.graphing
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 import org.nwapw.abacus.Abacus
 import org.nwapw.abacus.config.Configuration
+import org.nwapw.abacus.context.EvaluationContext
+import org.nwapw.abacus.context.MutableEvaluationContext
 import org.nwapw.abacus.number.NaiveNumber
 import org.nwapw.abacus.number.NumberInterface
 import org.nwapw.abacus.plugin.StandardPlugin
@@ -29,5 +34,14 @@ class Graph(val abacus: Abacus,
     init {
         this.expression = expression
         this.pointExpression = pointExpression
+    }
+
+    fun <T> evaluateWith(tree: TreeNode, values: List<T>,
+                        contextModifier: MutableEvaluationContext.(T) -> Unit) = runBlocking {
+        values.map {
+            val context = abacus.context.mutableSubInstance()
+            context.contextModifier(it)
+            async(CommonPool) { abacus.evaluateTreeWithContext(tree, context) }
+        }.map { it.await() }
     }
 }
